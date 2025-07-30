102fd8283e36a4ecf270edb12300ddc068ca420e
Khoa1512 <danggkhoaa1512@gmail.com>
2025-07-30 15:42:01 +0700

    Add telemetry selection widgets

---
+++ lib/presentation/widget/telemetry/telemetry_item_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:skylink/data/telemetry_data.dart';
+
+class TelemetryItemWidget extends StatelessWidget {
+  final TelemetryData telemetry;
+  final VoidCallback onTap;
+
+  const TelemetryItemWidget({
+    super.key,
+    required this.telemetry,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey.shade800.withValues(alpha: 0.5),
+            border: Border.all(
+              color: telemetry.color.withValues(alpha: 0.3),
+              width: 1,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                telemetry.label,
+                style: TextStyle(
+                  color: Colors.white.withValues(alpha: 0.8),
+                  fontSize: 11,
+                  fontWeight: FontWeight.w500,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              SizedBox(height: 4),
+              Row(
+                children: [
+                  Flexible(
+                    child: Text(
+                      telemetry.value,
+                      style: TextStyle(
+                        color: telemetry.color,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.5,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  if (telemetry.unit.isNotEmpty) ...[
+                    SizedBox(width: 2),
+                    Text(
+                      telemetry.unit,
+                      style: TextStyle(
+                        color: Colors.grey.shade400,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

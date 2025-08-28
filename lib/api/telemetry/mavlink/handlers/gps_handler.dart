5eaee90fdf76eb1b04fcd3dc056421e2ed922758
Khoa1512 <danggkhoaa1512@gmail.com>
2025-08-28 10:30:02 +0700

    Enhance GPS and position data handling accuracy

--- lib/api/telemetry/mavlink/handlers/gps_handler.dart
+++ lib/api/telemetry/mavlink/handlers/gps_handler.dart
@@ -1,22 +1,44 @@
 import 'package:dart_mavlink/dialects/common.dart';
+// import 'package:flutter/foundation.dart';
 import '../events.dart';
 
 class GpsHandler {
   final void Function(MAVLinkEvent) emit;
   GpsHandler(this.emit);
 
+  // Debug tracking (commented out)
+  // static DateTime? _lastGpsTime;
+  // static int _gpsMessageCount = 0;
+
   void handle(GpsRawInt msg) {
-    emit(MAVLinkEvent(MAVLinkEventType.gpsInfo, {
-      'fixType': _getGpsFix(msg.fixType),
-      'satellites': msg.satellitesVisible,
-      'lat': msg.lat / 1e7,
-      'lon': msg.lon / 1e7,
-      'alt': msg.alt / 1000.0,
-      'eph': msg.eph / 100.0,
-      'epv': msg.epv / 100.0,
-      'vel': msg.vel / 100.0,
-      'cog': msg.cog / 100.0,
-    }));
+    // Debug: Track GPS message frequency
+    // if (kDebugMode) {
+    //   DateTime now = DateTime.now();
+    //   _gpsMessageCount++;
+    //
+    //   if (_lastGpsTime != null) {
+    //     int intervalMs = now.difference(_lastGpsTime!).inMilliseconds;
+    //     if (_gpsMessageCount % 20 == 0) { // Log every 20 messages
+    //       double frequency = 1000.0 / intervalMs;
+    //       print('GPS_RAW_INT frequency: ${frequency.toStringAsFixed(1)}Hz (interval: ${intervalMs}ms)');
+    //     }
+    //   }
+    //   _lastGpsTime = now;
+    // }
+
+    emit(
+      MAVLinkEvent(MAVLinkEventType.gpsInfo, {
+        'fixType': _getGpsFix(msg.fixType),
+        'satellites': msg.satellitesVisible,
+        'lat': msg.lat / 1e7,
+        'lon': msg.lon / 1e7,
+        'alt': msg.alt / 1000.0,
+        'eph': msg.eph / 100.0,
+        'epv': msg.epv / 100.0,
+        'vel': msg.vel / 100.0,
+        'cog': msg.cog / 100.0,
+      }),
+    );
   }
 
   String _getGpsFix(int fixType) {
